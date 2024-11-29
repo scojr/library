d943b582522c9ed14fb06a2358bcc840c51d3d23
scojr <180873402+scojr@users.noreply.github.com>
2024-11-28 19:05:17 -0600

    Add edit functionality for existing book entries

--- javascript.js
+++ javascript.js
@@ -18,7 +18,6 @@ function updateLibrary() {
     title.textContent = book.title;
     title.className = "book-title";
 
-
     const author = document.createElement("h3");
     if (book.author) {
       author.textContent = `by ${book.author}`;
@@ -26,7 +25,6 @@ function updateLibrary() {
       author.textContent = "";
     }
     author.className = "book-author";
-
 
     const pageCount = document.createElement("p");
     if (book.pageCount) {
@@ -42,6 +40,7 @@ function updateLibrary() {
 
     const options = document.createElement("button");
     options.className = "options-button";
+    options.addEventListener("click", openBookForm);
 
     const optionsIcon = document.createElement("img");
     optionsIcon.setAttribute("src", "./images/feather.svg");
@@ -52,18 +51,14 @@ function updateLibrary() {
     entry.append(title, author, pageCount, status, options)
     entry.classList.add("book", book.status ? "status-read" : "status-unread");
 
-    const bookTitle = book.title;
-    const bookID = bookTitle.replace(/ /g, "-").toLowerCase();
-    entry.setAttribute('id', bookID)
+    entry.setAttribute('id', filterBookName(book.title))
     shelf.appendChild(entry);
   }
   const addBookButton = document.createElement("div");
   addBookButton.textContent = "+";
   addBookButton.className = ("class", "add-book-button");
   shelf.appendChild(addBookButton);
-  addBookButton.addEventListener("click", function (e) {
-    addBookModal.style.visibility = "visible";
-  });
+  addBookButton.addEventListener("click", openBookForm);
 }
 
 function clearLibrary() {
@@ -75,11 +70,20 @@ function clearLibrary() {
 
 }
 
+function filterBookName(name, formatted) {
+  if (formatted) {
+    return name.replace(/[^a-zA-Z0-9 ]/g, "");
+  } else {
+    return name.replace(/ /g, "-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "");
+  }
+}
+
 function Book(title, author, pageCount, status) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.status = status;
+  this.ID = filterBookName(title);
 }
 
 Book.prototype.info = function () {
@@ -92,6 +96,7 @@ function addBookToLibrary(title, author, pageCount, status) {
   updateLibrary();
 }
 
+
 const bookFormSubmit = document.querySelector(".submit-button");
 bookFormSubmit.addEventListener("click", takeFormInput);
 
@@ -101,30 +106,57 @@ function takeFormInput(event) {
   const requiredLabel = document.querySelector(".required-label")
   inputState = input.validity;
 
-  if (inputState.valid) {
-    event.preventDefault();
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const pageCount = document.querySelector("#pages").value;
-    const status = document.querySelector("#status").checked;
-    console.log(title, author, pageCount, status);
-    addBookToLibrary(title, author, pageCount, status);
-    updateLibrary();
-    closeBookForm();
+  if (editBookEntry.title === document.querySelector("#title").value) {
+    requiredLabel.style.setProperty('--duplicate-title-warning-visiblity', 'visible');
+    input.style.border = "1px solid red"
   } else {
-    requiredLabel.style.setProperty('--pseudo-visibility', 'visible');
-    input.style.border = "1px solid rgb(200, 0, 0)"
+    if (inputState.valid) {
+      if (editBookEntry) {
+        editBookEntry.title = filterBookName(document.querySelector("#title").value, true);
+        editBookEntry.author = filterBookName(document.querySelector("#author").value, true);
+        editBookEntry.pageCount = document.querySelector("#pages").value;
+        editBookEntry.status = document.querySelector("#status").checked;
+        editBookEntry.ID = filterBookName(document.querySelector("#title").value);
+        updateLibrary();
+        closeBookForm();
+      } else {
+        event.preventDefault();
+        const title = document.querySelector("#title").value;
+        const author = document.querySelector("#author").value;
+        const pageCount = document.querySelector("#pages").value;
+        const status = document.querySelector("#status").checked;
+        console.log(title, author, pageCount, status);
+        addBookToLibrary(title, author, pageCount, status);
+        updateLibrary();
+        closeBookForm();
+      }
+    } else {
+      requiredLabel.style.setProperty('--required-warning-visibility', 'visible');
+      input.style.border = "1px solid red"
+    }
   }
+}
+
+let editBookEntry = "";
+function openBookForm(event) {
+  if (event.currentTarget.className == "options-button") {
+    editBookEntry = myLibrary.find((book) => book.ID === event.currentTarget.parentElement.id);
+    document.querySelector("#title").value = editBookEntry.title;
+    document.querySelector("#author").value = editBookEntry.author;
+    document.querySelector("#pages").value = editBookEntry.pageCount;
+    document.querySelector("#status").checked = editBookEntry.status;
+  }
+  addBookModal.style.visibility = "visible";
 }
 
 const addBookModal = document.querySelector(".modal");
 const addBookForm = document.querySelector(".add-book-form");
-
 const bookFormClose = document.querySelector(".close");
 const bookFormCancel = document.querySelector(".cancel-button");
 
 bookFormClose.addEventListener("click", closeBookForm);
-addBookModal.addEventListener("click", () => {
+
+addBookModal.addEventListener("mousedown", () => {
   if (!addBookForm.matches(':hover')) {
     closeBookForm();
   }
@@ -135,10 +167,13 @@ bookFormCancel.addEventListener("click", (event) => {
 });
 
 function closeBookForm() {
+  editBookEntry = "";
   addBookModal.style.visibility = "hidden";
   addBookForm.reset();
   const requiredLabel = document.querySelector(".required-label")
-  requiredLabel.style.setProperty('--pseudo-visibility', 'hidden');
+  requiredLabel.style.setProperty('--duplicate-title-warning-visiblity', 'hidden');
+  requiredLabel.style.setProperty('--required-warning-visibility', 'hidden');
+  document.querySelector("#title").style.border = "1px solid var(--charcoal-black)";
 }
 
 console.log(myLibrary[0].info());
